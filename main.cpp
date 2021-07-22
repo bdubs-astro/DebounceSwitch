aa198d76574a5bd4e9641c0517a87e4a56612ea3
bdubs-astro <83251604+bdubs-astro@users.noreply.github.com>
2021-07-22 12:41:42 -0400

    Update main.cpp
    Modify print statements, comments.

--- main.cpp
+++ main.cpp
@@ -1,32 +1,31 @@
 #include <Arduino.h>
 #include "DebounceSwitch.h"
 
-// I/O
+// I/O ...
 #define swPin 14                // ESP8266 D5
-#define intPullup false   
+#define intPullup false         // use internal pullup resistor?
+int debounceDelay = 100;        // (ms)
+void loCallback(), hiCallback();
+DebounceSwitch mySwitch(swPin, intPullup, loCallback, hiCallback);
 #ifndef LED_BUILTIN 
 #define LED_BUILTIN 2           // ESP8266
 #endif
-
-// Switch stuff ...
-int debounceDelay = 100;        // (ms)
-void loCallback();
-void hiCallback();
-DebounceSwitch mySwitch(swPin, intPullup, loCallback, hiCallback);
 
 void setup() {
   // establish serial comms ...
   Serial.begin(9600);
   while(!Serial) {;}                // wait for connection
-  Serial.printf("\n\nSerial monitor connected to ESP8266.\n\n");
+  Serial.printf("\n\nSerial monitor connected.\n\n");
 
-  // configure LED ...
+  // configure indicator LED ...
   pinMode(LED_BUILTIN, OUTPUT); 
 
   // initialize switch ...
   bool swStateInit = mySwitch.initPin(); 
+  digitalWrite(LED_BUILTIN, !swStateInit);    // ESP8266 built-in LED is active LOW
+  
   const char *swStateStr[] = {"LOW", "HIGH"};
-  Serial.printf("\tInitial switch state: %s.\n\n", swStateStr[swStateInit]); 
+  Serial.printf("\tInitial input state is %s.\n\n", swStateStr[swStateInit]); 
 }
 
 void loop() {
@@ -35,11 +34,11 @@ void loop() {
 
 // callback functions ...
 void loCallback() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  Serial.printf("\tSwitch went LOW.\n\n"); 
+  digitalWrite(LED_BUILTIN, !LOW);          // ESP8266 built-in LED is active LOW
+  Serial.printf("\tInput just went LOW.\n\n"); 
 }
 
 void hiCallback() {
-  digitalWrite(LED_BUILTIN, LOW);
-  Serial.printf("\tSwitch went HIGH.\n\n");
+  digitalWrite(LED_BUILTIN, !HIGH);         // ESP8266 built-in LED is active LOW
+  Serial.printf("\tInput just went HIGH.\n\n");
 }
