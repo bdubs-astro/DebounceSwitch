e81f30c12d0261db8296db7f91d98cee1ef8b36e
Dr. Asshole <bdwissman@gmail.com>
2021-08-26 22:29:53 -0400

    Update submodule.

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+#include "DebounceSwitch.h"
+
+// I/O ...
+#define swPin 14                // ESP8266 D5
+#define intPullup false         // use internal pullup resistor?
+void loCallback(), hiCallback();
+DebounceSwitch mySwitch(swPin, intPullup, loCallback, hiCallback);
+#ifndef LED_BUILTIN 
+#define LED_BUILTIN 2           // ESP8266
+#endif
+
+void setup() {
+  // establish serial comms ...
+  Serial.begin(9600);
+  while(!Serial) {;}                // wait for connection
+  Serial.printf("\n\nSerial monitor connected.\n\n");
+
+  // configure indicator LED ...
+  pinMode(LED_BUILTIN, OUTPUT); 
+
+  // initialize switch ...
+  bool swStateInit = mySwitch.initPin(); 
+  digitalWrite(LED_BUILTIN, !swStateInit);  // ESP8266 built-in LED is active LOW
+
+  const char *swStateStr[] = {"LOW", "HIGH"};
+  Serial.printf("\tInitial input state is %s.\n\n", swStateStr[swStateInit]); 
+}
+
+void loop() {
+  int debounceDelay = 100;        // (ms)
+  bool swState = mySwitch.readPin(debounceDelay); 
+}
+
+// callback functions ...
+void loCallback() {
+  digitalWrite(LED_BUILTIN, !LOW);    // ESP8266 built-in LED is active LOW
+  Serial.printf("\tInput just went LOW.\n\n"); 
+}
+
+void hiCallback() {
+  digitalWrite(LED_BUILTIN, !HIGH);   // ESP8266 built-in LED is active LOW
+  Serial.printf("\tInput just went HIGH.\n\n");
+}
